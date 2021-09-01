1b9961b695ce2e7965386483ca8c88fb04d9f6e5
a4e3g6o0 <a4e3g6o0@hotmail.com>
2021-08-31 22:40:40 -0600

    Add Landing page, mobile view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 import { SimpleCardComponent } from './simple-card/simple-card.component';
 
 const routes: Routes = [
   { path: 'simple-card', component: SimpleCardComponent },
+  { path: 'landing-page', component: LandingPageComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SimpleCardComponent } from './simple-card/simple-card.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SimpleCardComponent
+    SimpleCardComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
