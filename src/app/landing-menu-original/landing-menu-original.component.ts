2e9e97122983e5a8edfac92ff621ea3fda119cc2
a4e3g6o0 <a4e3g6o0@hotmail.com>
2021-11-11 20:41:24 -0600

    Finish the original responsive page

--- src/app/landing-menu-original/landing-menu-original.component.ts
+++ src/app/landing-menu-original/landing-menu-original.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-landing-menu-original',
   templateUrl: './landing-menu-original.component.html',
-  styleUrls: ['./landing-menu-original.component.css']
+  styleUrls: ['./landing-menu-original.component.css'],
 })
 export class LandingMenuOriginalComponent implements OnInit {
+  openMenu = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
